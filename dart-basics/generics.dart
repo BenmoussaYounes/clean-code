b93b6cb47a2dc0c1d430f28b979413a97be8ba7d
younes <devbenmoussayounes@gmail.com>
2024-03-08 23:11:47 +0100

    feat(dart-basics) : add generics code sample

---
+++ dart-basics/generics.dart
@@ -0,0 +1,43 @@
+/*
+Generics are a way to make the code more reusable and type-safe.
+Generics allow you to define a class, function, or interface that can work with any data type.
+Generics are used to create reusable components that can work with any data type and provide type safety.
+
+*/
+
+void main() {
+  print(doTypeMatch(4, 1.5));
+  print(doTypeMatch(4, ''));
+  print(doTypeMatch(4, 4));
+
+  double a = eitherIntOrdDouble();
+  int b = eitherIntOrdDouble();
+
+  sum add = (num a, num b) => a + b;
+}
+
+// Example 1: Generic Function
+// A generic function is a function that can work with any data type.
+// The syntax for a generic function is to use angle brackets (<>) to define the type parameter.
+// The type parameter is a placeholder for the actual data type.
+
+// Here is an examples of generic functions:
+
+bool doTypeMatch<L, R>(L l, R r) => L == R;
+
+T eitherIntOrdDouble<T extends num>() {
+  switch (T) {
+    case int:
+      print('This is an integer');
+      return 1 as T;
+    case double:
+      print('This is a double');
+      return 1.0 as T;
+
+    default:
+      print("This is not a number");
+      return 0 as T;
+  }
+}
+
+typedef sum<T extends num> = T Function(T, T);
